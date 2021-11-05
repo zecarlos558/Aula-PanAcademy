f52febdddc2634396cb9fc5fbfb53a1732e00481
zecarlos558 <45573159+zecarlos558@users.noreply.github.com>
2021-11-05 16:23:23 -0300

    Atividade em grupo - SpringBoot + MongoDB
    Atividade realizada em grupo passada em sala de aula . Para realizar a inserção e exibição de médicos e suas respectivas agendas de consulta.
    Feita pelo Grupo 6

---
+++ SpringBootExercicios/hospitalPAN/src/main/java/com/exemplo/hospitalPAN/HospitalPanApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.hospitalPAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalPanApplication.class, args);
+	}
+
+}
---
+++ SpringBootExercicios/hospitalPAN/src/test/java/com/exemplo/hospitalPAN/HospitalPanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.hospitalPAN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
